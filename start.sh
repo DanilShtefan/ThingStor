650a2ce282a0e5ccf1a2e902f4c153fca7fb9940
DanilShtefan <danilshtefan.66@gmail.com>
2026-05-28 02:26:02 +0300

    fix: add createsuperuser to start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput
-python manage.py createcachetable --noinput 2>/dev/null || true
+DJANGO_SUPERUSER_PASSWORD=$SUPERUSER_PASSWORD python manage.py createsuperuser \
+  --username $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL --noinput 2>/dev/null || true
 gunicorn thingstor.wsgi:application --workers 4 --threads 2 --timeout 60 --access-logfile -
