c3f5e0dd57036f2a666d98449c840bab703492ed
DanilShtefan <danilshtefan.66@gmail.com>
2026-05-28 02:42:51 +0300

    fix: serve media files on Render (not just DEBUG)

--- thingstor/urls.py
+++ thingstor/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from decouple import config
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +16,7 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG or config('RENDER', default='false').lower() == 'true':
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
     urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
